81a3bd1045c7d6c77309c4b4016d4f7824c4c10f
Anthony Mai <anthony.mai530@gmail.com>
2019-12-09 02:03:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,8 +4,16 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  attr_accessor :emails
-  def parse(emails)
-    emails.split(/[," "]/)
-end
+  attr_accessor :emails, csv_emails
+  
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end
+  
+  def parse
+    csv_emails.split.collect do |address|
+      address.split(',')
+    end
+    .flatten.uniq
+  
 end
